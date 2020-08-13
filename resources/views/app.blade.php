2076a3b508f178b37953b1f419a4f26f24b0cef2
kodejuice <kodejuice@gmail.com>
2020-08-13 13:45:09 +0100

    ...

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="{{ asset("css/app.css") }}">
 
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <title>PhotApp</title>
     </head>
     <body>
         <div id="root"> </div>
