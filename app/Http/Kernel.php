d6b90ba0ac65fa83ce35ecc59504d8a9459ec835
kodejuice <kodejuice@gmail.com>
2020-08-13 16:15:05 +0100

    remove noisy middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,8 +22,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 
-        \App\Http\Middleware\ForceJsonResponse::class,
-        \App\Http\Middleware\Cors::class,
+        // \App\Http\Middleware\ForceJsonResponse::class,
+        // \App\Http\Middleware\Cors::class,
     ];
 
     /**
