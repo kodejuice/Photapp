3d0182aac3c41e53ce0f82e27a435ee4f8ccc474
kodejuice <kodejuice@gmail.com>
2020-08-15 16:50:27 +0100

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="{{ asset("css/app.css") }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
 
         <title>PhotApp</title>
     </head>
