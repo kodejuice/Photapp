c13b313acd9aed39efb9b7608aadae193dc02e3b
kodejuice <kodejuice@gmail.com>
2020-08-16 22:52:22 +0100

    setup page route

--- resources/js/components/App.tsx
+++ resources/js/components/App.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
-    const name: string = "fucking";
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">A {name} Component</div>
+import NotFound from './NotFound';
 
-                        <div className="card-body">I'm :just: a component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Splash from './Splash';
+
+const App: React.FC<{}> = ()=>{
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(()=>{
+        setMounted(true);
+    });
+
+    // display a splash screen if not mounted yet
+    return !mounted ?
+        (
+            <Splash color='grey' />
+        )
+        :
+        (
+            <main>
+                <Switch>
+                    <Route path="/" component={Home} exact />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                    {/*<Route path="/:username" component={UserProfile} />*/}
+                    <Route component={NotFound} />
+                </Switch>
+            </main>
+        )
 }
 
 
