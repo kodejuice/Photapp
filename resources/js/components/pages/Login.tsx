5a22c0e333d78e96569fd97438208040526e5f83
kodejuice <kodejuice@gmail.com>
2020-08-16 22:50:24 +0100

    setup

--- resources/js/components/pages/Login.tsx
+++ resources/js/components/pages/Login.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useHistory } from "react-router-dom";
+import {useSelector} from 'react-redux';
+import Cookie from 'js-cookie';
+import '../styles/auth-page.scss';
+
 
 const Login: React.FC<{}> = () => {
-    const name: string = "Login";
+    const history = useHistory();
+
+    if (Cookie.get("AUTH_TOKEN")) {
+        // user already logged in, redirect to home page
+        history.push("/");
+        return;
+    }
 
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">A {name} Component</div>
-
-                        <div className="card-body">I'm :just: a component!</div>
+        <div>
+            <div className='card login-card auth-pg'>
+                <div className="card-body">
+                    <div className="brand-title">
+                        <h1 className="card-title"></h1>
                     </div>
+
+                    <div className="login-box">
+                        <form className="form-group">
+                            <input type="text" placeholder="Username" id="user" name='username' required={true}/>
+                            <input type="password" placeholder="Password" id="pass" name='password' required={true}/>
+                            <button type="submit" className='btn btn-block btn-small btn-secondary'> Log in </button>
+                        </form>
+
+                        <div className="separate-or"> or </div>
+
+                        <div className="alt-btns">
+                            <div><a className="guest link" onClick={_=>history.push("/")}> Enter as Guest </a></div>
+                            <div><a className="forgot-pass link" onClick={_=>history.push("/password-reset")}> Forgot password? </a></div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className='card signup-request-card auth-pg'>
+                <div className='card-body'>
+                    <p> Don't have an account? <a className="link" onClick={_=>history.push("/register")}>signup</a> </p>
                 </div>
             </div>
         </div>
--- resources/js/components/pages/Register.tsx
+++ resources/js/components/pages/Register.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
+import '../styles/auth-page.scss';
+
 
 const Register: React.FC<{}> = () => {
-    const name: string = "Register";
+    const history = useHistory();
 
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">A {name} Component</div>
-
-                        <div className="card-body">I'm :just: a component!</div>
+        <div>
+            <div className='card register-card auth-pg'>
+                <div className="card-body">
+                    <div className="brand-title">
+                        <h1 className="card-title"></h1>
                     </div>
+
+                    <div className="login-box">
+                        <form className="form-group" onSubmit={_=>_}>
+                            <input type="email" placeholder="Email" id="email" name='email' required={true}/>
+                            <input type="text" placeholder="Full name" id="name" name='full_name' />
+                            <input type="text" placeholder="Username" id="user" name='username' required={true}/>
+                            <input type="password" placeholder="Password" id="pass" name='password' required={true}/>
+                            <button type='submit' className='btn btn-block btn-small btn-secondary' disabled={!true}> Sign up </button>
+                        </form>
+
+                        <div className="alt-btns">
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className='card signup-request-card auth-pg'>
+                <div className='card-body'>
+                    <p> Have an account? <a className="link" onClick={_=>history.push("/login")}>Log in</a> </p>
                 </div>
             </div>
         </div>
     );
+
 }
 
 
