a6e9a7b6d80066d76ab35df0497718b5be2e6922
kodejuice <kodejuice@gmail.com>
2020-08-17 17:18:30 +0100

    login

--- resources/js/components/pages/Login.tsx
+++ resources/js/components/pages/Login.tsx
@@ -1,18 +1,44 @@
 import React, {useEffect, useState} from 'react';
-import { useHistory } from "react-router-dom";
-import {useSelector} from 'react-redux';
+import { useHistory, Link } from "react-router-dom";
+import axios from 'axios';
 import Cookie from 'js-cookie';
+import { useForm } from "react-hook-form";
+import nprogress from '../../routes/nprogress';
+
+import Splash from '../Splash';
 import '../styles/auth-page.scss';
+
+
+// TODO: setup react-hook-form
+
+type Inputs = {
+    username: string,
+    password: string,
+};
+
+const errorProps: (err) => object|undefined = (err) => {
+    return err && {
+        className: 'border border-danger',
+    };
+};
+
 
 
 const Login: React.FC<{}> = () => {
     const history = useHistory();
+    const [errs, setErrs] = useState([]);
+    const { register, handleSubmit, watch, errors } = useForm<Inputs>();
 
-    if (Cookie.get("AUTH_TOKEN")) {
-        // user already logged in, redirect to home page
-        history.push("/");
-        return;
+    const onComplete: (d:Inputs)=>void = d => {
+        UserSignin(d, setErrs, history);
     }
+
+    // user already logged in?, redirect to home page
+    if (Cookie.get("AUTH_TOKEN")) {
+        // (window as any).location = "/";
+        // return <Splash color='grey' />;
+    }
+
 
     return (
         <div>
@@ -23,31 +49,105 @@ const Login: React.FC<{}> = () => {
                     </div>
 
                     <div className="login-box">
-                        <form className="form-group">
-                            <input type="text" placeholder="Username" id="user" name='username' required={true}/>
-                            <input type="password" placeholder="Password" id="pass" name='password' required={true}/>
+                        <form className="form-group" onSubmit={handleSubmit(onComplete)}>
+
+                            <input type="text" placeholder="Username" id="user" name='username'
+                                ref={register({required:true})}
+                                {...errorProps(errors.username)}
+                            />
+
+                            <input type="password" placeholder="Password" id="pass" name='password'
+                                ref={register({required:true})}
+                                {...(errorProps(errors.password))}
+                            />
+
                             <button type="submit" className='btn btn-block btn-small btn-secondary'> Log in </button>
                         </form>
 
                         <div className="separate-or"> or </div>
 
                         <div className="alt-btns">
-                            <div><a className="guest link" onClick={_=>history.push("/")}> Enter as Guest </a></div>
-                            <div><a className="forgot-pass link" onClick={_=>history.push("/password-reset")}> Forgot password? </a></div>
+                            <div id='guest'><Link to="/"> Enter as Guest </Link></div>
+                            <div id='pass-reset'><Link to="/password-reset"> Forgot password? </Link></div>
                         </div>
-
                     </div>
                 </div>
             </div>
 
+            {/* any errors? */}
+            {errs.length ?
+                (
+                    <div className="auth-pg-error-alert">
+                        {errs.map((el) => <div key={el}> {el} </div>)}
+                    </div>
+                ) : ""
+            }
+
             <div className='card signup-request-card auth-pg'>
                 <div className='card-body'>
-                    <p> Don't have an account? <a className="link" onClick={_=>history.push("/register")}>signup</a> </p>
+                    <p> Don't have an account? <Link to="/register">Sign Up</Link> </p>
                 </div>
             </div>
         </div>
     );
 }
 
+
+/**
+ * handle login submission
+ */
+async function UserSignin({username, password}: Inputs, setErrs, history) {
+    nprogress.start();
+    setErrs([]);
+
+    let req;
+    try {
+        req = await axios.post('/api/login', {
+            username,
+            password
+        });
+
+        // not valid response? throw error
+        // this is better than axios rejecting the Promise, this way we can show
+        // a meaningful error
+        // @see `../../bootstrap.js`
+        //
+        if (req.status != 200 || !req.data?.token) {
+            throw req;
+        }
+
+        // store auth token cookie and redirect to home
+        storeCookie(req.data.token, ()=>{
+            history.push("/");
+        });
+    } catch (err) {
+        if (err?.data?.errors instanceof Array) {
+            // our error
+            setErrs(err.data.errors);
+        } else {
+            // other errors
+            if (typeof err?.data?.message == 'string') {
+                setErrs([err.data.message || "An unknown error occured"])
+            } else {
+                setErrs([err.toString()]);
+            }
+        }
+    }
+
+    nprogress.done();
+}
+
+
+/**
+ * Stores a cookie and invokes callback()
+ *
+ * @param      {string}    token     The auth token
+ * @param      {Function}  callback  The callback
+ */
+function storeCookie(token: string, callback: ()=>void) {
+    Cookie.set('AUTH_TOKEN', token);
+
+    callback();
+}
 
 export default Login;
