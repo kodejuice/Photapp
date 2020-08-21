ec2bbae2053fc9c85c336481f2effce8f9df8565
kodejuice <kodejuice@gmail.com>
2020-08-21 12:18:00 +0100

    rm

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -1,22 +1,3 @@
-import React from 'react';
-
-
-const Home: React.FC<{}> = ({children})=>{
-    const name: string = "homw";
-    
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">A {name} Component</div>
-
-                        <div className="card-body">I'm :just: a component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
 }
 
 
