be5a2f5a34e3d56e13f2aa79b4a878fcc3e4adf6
kodejuice <kodejuice@gmail.com>
2020-08-22 16:54:57 +0100

    desktop header

---
+++ resources/js/components/Header/desktop.tsx
@@ -0,0 +1,79 @@
+import React, {useRef, useState} from 'react';
+import { useHistory, Link } from "react-router-dom";
+import {useSelector} from 'react-redux';
+import {RootState} from '../../state/store';
+import {userProfile as profileObject} from '../../state/userProfile.d';
+
+
+const Header: React.FC<{}> = ()=>{
+    const ref = useRef(null);
+    const history = useHistory();
+
+    const [dropdown_shown, showDropdown] = useState(false) ;
+    const logged_in = useSelector<RootState, boolean>(({isLogged}) => isLogged);
+    const user = useSelector<RootState, profileObject>(({userProfile}) => userProfile);
+
+    return (
+        <header className='hide-mobile'>
+            <nav className="header border fixed">
+                <div className="header-wrapper row">
+                    <div className="header-brand col-3 col">
+                        <Link to="/"><h1 className="bg-logo"> </h1></Link>
+                    </div>
+                    <div className="search-bar col-3 col hide-width-814">
+                        <div className='search-input-wrap'>
+                            <form onSubmit={_=>onSearch(_, ref.current, history)}>
+                                <input ref={ref} type='search' placeholder='Search' name='search' />
+                            </form>
+                        </div>
+                    </div>
+                    <div className="nav-links col-fill col">
+                        <ul className="inline">
+                            <li><Link to="/"><img src="/icon/home.png"/></Link></li>
+                            <li><Link to="?feed=following"><img src="/icon/feed.png"/></Link></li>
+                            <li><Link to="/activity"><img src="/icon/heart.png"/></Link></li>
+                            {
+                                !user?.id ?
+                                    <li>
+                                        <Link to="/login"><img src="/icon/login.png"/></Link>
+                                    </li>
+                                    :
+                                    <li>
+                                        <div className="dropdown">
+                                            <img onClick={_=>showDropdown(!dropdown_shown)}
+                                                 className='dropbtn avatar'
+                                                 src={user.profile_pic ? `/avatar/${user.profile_pic}` : '/icon/avatar.png'}
+                                            />
+                                            <div className={`dropdown-content ${dropdown_shown?'show':''}`}>
+                                                <Link to={`/user/${user.username}`}> Profile</Link>
+                                                <Link to={`/user/${user.username}?tab=saved`}> Saved</Link>
+                                                <Link to={`/user/${user.username}/settings`}> Settings</Link>
+                                                <Link to={`/user/${user.username}?tab=saved`}>Logout</Link>
+                                            </div>
+                                        </div>
+                                    </li>
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+
+/**
+ * Redirects user to search page with query.
+ *
+ */
+function onSearch(ev: React.SyntheticEvent<HTMLFormElement>, input: HTMLInputElement|null, history) {
+    ev.preventDefault();
+    const query = input?.value;
+
+    if (query) {
+        history.push(`/search?q=${query}`);
+    }
+}
+
+
+export default Header;
--- resources/js/components/Header/index.tsx
+++ resources/js/components/Header/index.tsx
@@ -1,68 +1,25 @@
-import React, {useRef} from 'react';
-import { useHistory, Link } from "react-router-dom";
-import {useSelector} from 'react-redux';
-import {RootState} from '../../state/store';
-import {userProfile as profileObject} from '../../state/userProfile.d';
+import React, {useRef, useState} from 'react';
+
+import HeaderDesktop from './desktop';
+import HeaderMobile from './mobile';
 
 import './style.scss';
 
 
-export default function Header() {
-    const ref = useRef(null);
-    const history = useHistory();
- 
-    const logged_in = useSelector<RootState, boolean>(({isLogged}) => isLogged);
-    const user = useSelector<RootState, profileObject>(({userProfile}) => userProfile);
+const Header: React.FC<{}> = () => {
+
 
     return (
-        <nav className="header border fixed">
-            <div className="header-wrapper row">
-                <div className="header-brand col-3 col">
-                    <Link to="/"><h1 className="bg-logo"> </h1></Link>
-                </div>
-                <div className="search-bar col-3 col hide-width-814">
-                    <div className='search-input-wrap'>
-                        <form onSubmit={_=>onSearch(_, ref.current, history)}>
-                            <input ref={ref} type='search' placeholder='Search' name='search' />
-                        </form>
         <React.Fragment>
-                    </div>
-                </div>
-                <div className="nav-links col-fill col">
-                    <ul className="inline">
-                        <li><Link to="/"><img src="/icon/home.png"/></Link></li>
-                        <li><Link to="?feed=following"><img src="/icon/feed.png"/></Link></li>
-                        <li><Link to="/activity"><img src="/icon/heart.png"/></Link></li>
-                        {
-                            !user?.id ?
-                                <li>
-                                    <Link to="/login"><img src="/icon/login.png"/></Link>
-                                </li>
-                                : <li>
-                                    <Link to={`/user/${user.username}`}><img className='avatar' src={`/avatar/${user.profile_pic}`} /></Link>
-                                  </li>
-                        }
-                    </ul>
-                </div>
-            </div>
-        </nav>
+
+            <HeaderDesktop />
+
+            <HeaderMobile />
+
         </React.Fragment>
+
   );
 
 }
 
-
-/**
- * Redirects user to search page with query.
- *
- */
-function onSearch(ev: React.SyntheticEvent<HTMLFormElement>, input: HTMLInputElement|null, history) {
-    ev.preventDefault();
-    const query = input?.value;
-
-    if (query) {
-        history.push(`/search?q=${query}`);
-    }
-}
-
-
+export default Header;
