1dcad9310c5116629e2c89cd78d7f9460a2f94b5
kodejuice <kodejuice@gmail.com>
2020-08-23 15:00:26 +0100

    remove splash

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -1,26 +1,17 @@
 import React, {useEffect, useState} from 'react';
 
 import Header from '../components/Header';
-import Splash from '../components/Splash';
+// import Splash from '../components/Splash';
 
 
 const Home: React.FC<{}> = ()=>{
     const [mounted, setMounted] = useState(false);
 
-    useEffect(()=>{
-        setMounted(true);
-    });
-
-    return !mounted ?
-        (
-            <Splash color='clr'/>
-        )
-        :
-        (
-            <div>
-                <Header page='/' />
-            </div>
-        )
+    return (
+        <React.Fragment>
+            <Header page='/' />
+        </React.Fragment>
+    );
 }
 
 
