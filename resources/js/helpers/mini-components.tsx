cd3d2f1d15ef0f35289f04c45474004d08e70735
kodejuice <kodejuice@gmail.com>
2020-08-25 20:42:04 +0100

    highlightMentions

--- resources/js/helpers/mini-components.tsx
+++ resources/js/helpers/mini-components.tsx
@@ -1,17 +1,65 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
-// TODO: implement this
-
-
-// make the links like this!
-// <a onClick={ev=>{ev.stopPropagation(); history.push(`/user/${d.associated_user}`)}}>
-
+// TODO: implement highlightTagsAndMentions
 
 /**
- * return a link to mentioned users in a message (@user -> <Link to='/user/...)
+ * return links to mentioned users in a string
+ * 
+ * e.g, "hi @mike" -> hi <a href='/user/mike'>mike</a>
+ *
+ * converts the string to JSXElements, so they actually appear as links
+ * when rendered
+ *
  * @param {string} str string
  */
 export function highlightMentions(str: string): React.ReactNode {
-    /* wrap in an linline element */
-    return <span> {str} </span>;
+    const history = useHistory();
+    let strs = str.split('@');
+    let res: {type:string,str:string}[] = [];
+
+    if (strs[0] != '') {
+        res.push({
+            type: 'text',
+            str: strs[0],
+        });
+    }
+
+    strs.shift();
+    let firstWord: string, s: string;
+    for (let i=0; i<strs.length; ++i) {
+        s = strs[i];
+
+        let match = s.match(/^(\w+)/);
+        if (match) {
+            // first word
+            res.push({
+                type: 'link',
+                str: match[0]
+            });
+
+            // ...rest
+            res.push({
+                type: 'text',
+                str: s.slice(match[0].length)
+            });
+        } else {
+            res.push({
+                type: 'text',
+                str: "@" + s
+            });
+        }
+    }
+
+    return res.map((o,i)=>{
+        if (o.type == 'text') {
+            return <span key={o.str}> {o.str} </span>;
+        } else {
+            return (
+                <a key={o.str} onClick={ev=>{ev.stopPropagation(); history.push(`/user/${o.str}`)}}>
+                    @{o.str}
+                </a>
+            );
+        }
+    });
 }
