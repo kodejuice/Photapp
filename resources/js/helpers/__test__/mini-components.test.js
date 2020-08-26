8519d4d9ac8a6556925c1aa86ba56f314da7c688
kodejuice <kodejuice@gmail.com>
2020-08-26 18:37:30 +0100

    ...

--- resources/js/helpers/__test__/mini-components.test.js
+++ resources/js/helpers/__test__/mini-components.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {render, fireEvent, screen} from '@testing-library/react';
 import "babel-polyfill";
 import '@testing-library/jest-dom';
 
