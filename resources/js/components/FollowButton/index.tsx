cdbf12047b64e4821dc36e8118110195d4d7a98e
kodejuice <kodejuice@gmail.com>
2020-08-29 22:06:54 +0100

    ...

--- resources/js/components/FollowButton/index.tsx
+++ resources/js/components/FollowButton/index.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 
 type FollowButtonProps = {
     user: string,
-    notification_id?: number|null
+    notification_id?: number|null,
+    unfollow?: boolean,
 }
 
-const FollowButton: React.FC<FollowButtonProps> = ({user, notification_id}) => {
+const FollowButton: React.FC<FollowButtonProps> = ({user, notification_id, unfollow}) => {
     return (
-        <button onClick={_=>followUser(user, notification_id)}> Follow </button>
+        unfollow
+        ?<button className='unfollow-button' onClick={_=>unfollowUser(user)}> Unfollow </button>
+        :<button onClick={_=>followUser(user, notification_id)}> Follow </button>
     );
 }
 
+
+function unfollowUser(user: string) {
+    // TODO: unfollow user
+}
 
 function followUser(user: string, notification_id?: number|null) {
     // TODO: follow user and set notification? as read
