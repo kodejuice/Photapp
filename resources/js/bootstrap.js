62c14c100ded09159068414378bef60aa501917b
kodejuice <kodejuice@gmail.com>
2020-09-06 10:57:59 +0100

    not needed

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,8 +3,7 @@ window._ = require('lodash');
 try {
     //window.Popper = require('popper.js').default;
     //window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
+    // require('bootstrap');
 } catch (e) {}
 
 /**
