59b53b6c1db46dafaa373c757a122de100e5aa79
kodejuice <kodejuice@gmail.com>
2020-09-08 14:03:54 +0100

    post info in transparent box

--- resources/js/components/Posts/GridView/index.tsx
+++ resources/js/components/Posts/GridView/index.tsx
@@ -5,11 +5,13 @@ import PhotoGrid from '../../PhotoGrid';
 const GridPosts: React.FC<{data:any[], view?:'tile'}> = ({data, view})=>{
     view = view || 'tile';
 
-    const posts_data = data.map(({media_type, post_id, post_url}) => ({
+    const posts_data = data.map(({media_type, post_id, post_url, like_count, comment_count}) => ({
+        post_id,
+        like_count,
+        comment_count,
         media_type: JSON.parse(media_type)[0],
         preview_image: JSON.parse(post_url)[0][1],
         multiple: JSON.parse(media_type).length > 1,
-        post_id,
     }));
 
     return <PhotoGrid data={posts_data} />;
