e35a909af3f52e23a9603c055c2383a24a07c2e6
kodejuice <kodejuice@gmail.com>
2020-09-16 18:10:31 +0100

    feat: profile settings modal

---
+++ resources/js/pages/account/User/edit_profile_modal.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {logUserOut} from '../../../helpers/fetcher';
+import {useDispatch} from 'react-redux';
+import showAlert from '../../../components/Alert/showAlert';
+
+export const EditProfileModal: React.FC<{}> = ()=>{
+    return (
+        <React.Fragment>
+            <input className="modal-state" id="modal-editprofile" type="checkbox"/>
+            <div className="modal">
+                <label className="modal-bg" htmlFor="modal-editprofile"></label>
+                <div className="modal-body edit-profile">
+                    <button> <Link to="/account/password/change">Change Password</Link> </button>
+                    <button> <Link to="/account/notifications">Notifications</Link> </button>
+                    <label htmlFor='modal-logout'> Log out </label>
+                    <label htmlFor='modal-editprofile'> Cancel </label>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+
+export const LogoutModal: React.FC<{}> = ()=>{
+    const dispatch = useDispatch();
+
+    const logOut = async ()=>{
+        await logUserOut(
+            () => {
+                showAlert(dispatch, ['Logging you out'], 'success');
+                location.reload();
+            },
+            (errs: Array<string>) => {
+                showAlert(dispatch, errs, 'error', 20);
+            }
+        );
+    }
+
+    return (
+        <React.Fragment>
+            <input className="modal-state" id="modal-logout" type="checkbox"/>
+            <div className="modal">
+                <label className="modal-bg" htmlFor="modal-logout"></label>
+                <div className="modal-body logout">
+                    <div className='header'> Logout? </div>
+                    <div className='row'>
+                        <div className='col col-fill'><button onClick={logOut}>Yes</button></div>
+                        <div className='col col-fill'><label htmlFor='modal-logout'>No</label></div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
--- resources/js/pages/account/User/index.tsx
+++ resources/js/pages/account/User/index.tsx
@@ -14,10 +14,12 @@ import FollowButton from '../../../components/FollowButton';
 import Spinner from '../../../components/Spinner';
 import {ProcessUserInput} from '../../../helpers/mini-components';
 
+import {EditProfileModal, LogoutModal} from './edit_profile_modal';
 import UserFollow from './user_follows';
 
 import "./styles.scss";
 
+const SettingsIcons = <svg aria-label="Options" fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path clipRule="evenodd" d="M46.7 20.6l-2.1-1.1c-.4-.2-.7-.5-.8-1-.5-1.6-1.1-3.2-1.9-4.7-.2-.4-.3-.8-.1-1.2l.8-2.3c.2-.5 0-1.1-.4-1.5l-2.9-2.9c-.4-.4-1-.5-1.5-.4l-2.3.8c-.4.1-.8.1-1.2-.1-1.4-.8-3-1.5-4.6-1.9-.4-.1-.8-.4-1-.8l-1.1-2.2c-.3-.5-.8-.8-1.3-.8h-4.1c-.6 0-1.1.3-1.3.8l-1.1 2.2c-.2.4-.5.7-1 .8-1.6.5-3.2 1.1-4.6 1.9-.4.2-.8.3-1.2.1l-2.3-.8c-.5-.2-1.1 0-1.5.4L5.9 8.8c-.4.4-.5 1-.4 1.5l.8 2.3c.1.4.1.8-.1 1.2-.8 1.5-1.5 3-1.9 4.7-.1.4-.4.8-.8 1l-2.1 1.1c-.5.3-.8.8-.8 1.3V26c0 .6.3 1.1.8 1.3l2.1 1.1c.4.2.7.5.8 1 .5 1.6 1.1 3.2 1.9 4.7.2.4.3.8.1 1.2l-.8 2.3c-.2.5 0 1.1.4 1.5L8.8 42c.4.4 1 .5 1.5.4l2.3-.8c.4-.1.8-.1 1.2.1 1.4.8 3 1.5 4.6 1.9.4.1.8.4 1 .8l1.1 2.2c.3.5.8.8 1.3.8h4.1c.6 0 1.1-.3 1.3-.8l1.1-2.2c.2-.4.5-.7 1-.8 1.6-.5 3.2-1.1 4.6-1.9.4-.2.8-.3 1.2-.1l2.3.8c.5.2 1.1 0 1.5-.4l2.9-2.9c.4-.4.5-1 .4-1.5l-.8-2.3c-.1-.4-.1-.8.1-1.2.8-1.5 1.5-3 1.9-4.7.1-.4.4-.8.8-1l2.1-1.1c.5-.3.8-.8.8-1.3v-4.1c.4-.5.1-1.1-.4-1.3zM24 41.5c-9.7 0-17.5-7.8-17.5-17.5S14.3 6.5 24 6.5 41.5 14.3 41.5 24 33.7 41.5 24 41.5z" fillRule="evenodd"></path></svg>;
 
 const UserPosts: React.FC<{username:string}> = ({username}) => {
     const dispatch = useDispatch();
@@ -101,6 +103,7 @@ const UserProfile: React.FC<Router.RouteComponentProps> = ({match, location})=>{
 
     const query = new URLSearchParams(location.search);
     const [tab, setTab] = useState<0|1|2>((isSelf && query.get('tab')=='saved') ? 1 : 0);
+                            // 0 -> Posts, 1 -> Saved, 2 -> Tagged
 
     let {data, isLoading, isError} = useUser(params.username);
 
@@ -119,6 +122,8 @@ const UserProfile: React.FC<Router.RouteComponentProps> = ({match, location})=>{
 
                 {data && (
                     <React.Fragment>
+
+                        {/*mobile*/}
                         <div className='mobile-profile-head'>
                             <div className='profile-info'>
                                 <div className='row info'>
@@ -128,16 +133,20 @@ const UserProfile: React.FC<Router.RouteComponentProps> = ({match, location})=>{
                                     <div className='col-1 hidden-col-dp'></div>
                                     <div className='col-9 username-col'>
                                         <div className='username'>
-                                            <p id='username'>{data.username}</p>
+                                            <p id='username'>
+                                                {data.username}
+                                                {isSelf && <label htmlFor='modal-editprofile'><div className='settings-icon-mobile'> {SettingsIcons} </div> </label>}
+                                            </p>
                                         </div>
                                         <div className='follow-button'>
                                             {logged && !isSelf && <FollowButton user={data.username} unfollow={data.auth_user_follows} />}
+                                            {isSelf && <Link to="/accounts/edit"><button className='edit-profile'>Edit Profile</button></Link>}
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <div className='bio'>
-                                <p id='full_name'> {data.full_name} </p>
+                                <p id='full_name'> {data.full_name}</p>
                                 <div className='profile-bio'>
                                     <p id='bio'> <ProcessUserInput text={limit(data.bio||"", 250)} /> </p>
                                 </div>
@@ -149,6 +158,7 @@ const UserProfile: React.FC<Router.RouteComponentProps> = ({match, location})=>{
                             </div>
                         </div>
 
+                        {/*desktop*/}
                         <div className='desktop-profile-head'>
                             <div className='profile-info'>
                                 <div className='row info'>
@@ -159,12 +169,14 @@ const UserProfile: React.FC<Router.RouteComponentProps> = ({match, location})=>{
 
                                     <div className='col col-fill'>
                                         <div className='row'>
-                                            <div className='col col-2'>
-                                                <p id='username'>{data.username}</p>
+                                            <div className='col col-3'>
+                                                <p id='username'>{limit(data.username, 15)}</p>
                                             </div>
                                             <div className='col col-1 hidden-col-username'></div>
-                                            <div className='col col-4 follow-button'>
+                                            <div className='col col-7 follow-button'>
                                                 {logged && !isSelf && <FollowButton user={data.username} unfollow={data.auth_user_follows}/>}
+                                                {isSelf && <Link to="/accounts/edit"><button className='edit-profile'>Edit Profile</button></Link>}
+                                                {isSelf && <label htmlFor='modal-editprofile'><div className='settings-icon'> {SettingsIcons} </div> </label>}
                                             </div>
                                         </div>
 
@@ -186,8 +198,8 @@ const UserProfile: React.FC<Router.RouteComponentProps> = ({match, location})=>{
                             </div>
                         </div>
 
-                        {/*TABs*/}
 
+                        {/*TABs*/}
                         <div className='tabs'>
                             <div className="row flex-spaces tabs">
                                 <input id="tab1" type="radio" name="tabs" defaultChecked={tab==0}/>
@@ -217,45 +229,51 @@ const UserProfile: React.FC<Router.RouteComponentProps> = ({match, location})=>{
 
 
                         {/* MODALS */}
-
                         {data.followers && (
-                            <div>
-                            <input className="modal-state" id="modal-followers" type="checkbox"/>
-                            <div className="modal">
-                                <label className="modal-bg" htmlFor="modal-followers"></label>
-                                <div className="modal-body">
-                                    <label className="btn-close" htmlFor="modal-followers">X</label>
-                                    <div className='row'>
-                                        <div className='col col-1' style={{padding: '0'}}></div>
-                                        <div className='page-title'> {`${data.username} followers`} </div>
+                            <React.Fragment>
+                                <input className="modal-state" id="modal-followers" type="checkbox"/>
+                                <div className="modal">
+                                    <label className="modal-bg" htmlFor="modal-followers"></label>
+                                    <div className="modal-body">
+                                        <label className="btn-close" htmlFor="modal-followers">X</label>
+                                        <div className='row'>
+                                            <div className='col col-1' style={{padding: '0'}}></div>
+                                            <div className='page-title'> {`${data.username} followers`} </div>
+                                        </div>
+                                        <UserFollow type='followers' username={data.username} />
                                     </div>
-                                    <UserFollow type='followers' username={data.username} />
                                 </div>
-                            </div>
-                            </div>
+                            </React.Fragment>
                         ) || ""}
 
                         {data.follows && (
-                            <div>
-                            <input className="modal-state" id="modal-following" type="checkbox"/>
-                            <div className="modal">
-                                <label className="modal-bg" htmlFor="modal-following"></label>
-                                <div className="modal-body">
-                                    <label className="btn-close" htmlFor="modal-following">X</label>
-                                    <div className='row'>
-                                        <div className='col col-1' style={{padding: '0'}}></div>
-                                        <div className='page-title'> {`${data.username} following`} </div>
+                            <React.Fragment>
+                                <input className="modal-state" id="modal-following" type="checkbox"/>
+                                <div className="modal">
+                                    <label className="modal-bg" htmlFor="modal-following"></label>
+                                    <div className="modal-body">
+                                        <label className="btn-close" htmlFor="modal-following">X</label>
+                                        <div className='row'>
+                                            <div className='col col-1' style={{padding: '0'}}></div>
+                                            <div className='page-title'> {`${data.username} following`} </div>
+                                        </div>
+                                        <UserFollow type='following' username={data.username} />
                                     </div>
-                                    <UserFollow type='following' username={data.username} />
                                 </div>
-                            </div>
-                            </div>
+                            </React.Fragment>
                         ) || ""}
+
+                        {isSelf && (
+                            <React.Fragment>
+                                <EditProfileModal />
+
+                                <LogoutModal />
+                            </React.Fragment>
+                        )}
 
                     </React.Fragment>
                 )}
             </div>
-
         </React.Fragment>
     );
 };
