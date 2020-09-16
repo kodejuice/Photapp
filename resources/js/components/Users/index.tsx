6a52617f73a064c76e7cbbc58d016a3a35fb46cf
kodejuice <kodejuice@gmail.com>
2020-09-16 13:33:01 +0100

    fix: limit username length

--- resources/js/components/Users/index.tsx
+++ resources/js/components/Users/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import authUser from '../../state/auth_user';
 
+import {limit} from '../../helpers/util';
 import LazyDP from '../../components/LazyDP';
 import FollowButton from '../../components/FollowButton';
 
@@ -16,8 +17,8 @@ const Users: React.FC<{data:any[],rdr?:boolean}> = ({data, rdr})=>{
                     <div role='user' className='user row' key={username}>
                         <div className='col col-2'> <Link target={rdr ? 'blank' : ""} to={`/user/${username}`}><LazyDP user={username} /></Link> </div>
                         <div className='col col-fill'>
-                            <div className='username'><Link target={rdr ? 'blank' : ""} to={`/user/${username}`}>{username}</Link></div>
-                            <div className='follow-info'>{(logged && follows_auth_user) ? "Follows You" : full_name}</div>
+                            <div className='username'><Link target={rdr ? 'blank' : ""} to={`/user/${username}`}>{limit(username, 17)}</Link></div>
+                            <div className='follow-info'>{(logged && follows_auth_user) ? "Follows You" : limit(full_name, 20)}</div>
                         </div>
                         <div className='col col-2 follow-col'>
                             {logged && username!=user.username && <FollowButton user={username} unfollow={auth_user_follows} />}
