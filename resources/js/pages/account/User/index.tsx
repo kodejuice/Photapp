5729d3a2edd1888f98ac480278de45dc48532ab9
kodejuice <kodejuice@gmail.com>
2020-09-17 13:03:09 +0100

    refactor(user profile): remove redundant components

--- resources/js/pages/account/User/index.tsx
+++ resources/js/pages/account/User/index.tsx
@@ -21,9 +21,10 @@ import "./styles.scss";
 
 const SettingsIcons = <svg aria-label="Options" fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path clipRule="evenodd" d="M46.7 20.6l-2.1-1.1c-.4-.2-.7-.5-.8-1-.5-1.6-1.1-3.2-1.9-4.7-.2-.4-.3-.8-.1-1.2l.8-2.3c.2-.5 0-1.1-.4-1.5l-2.9-2.9c-.4-.4-1-.5-1.5-.4l-2.3.8c-.4.1-.8.1-1.2-.1-1.4-.8-3-1.5-4.6-1.9-.4-.1-.8-.4-1-.8l-1.1-2.2c-.3-.5-.8-.8-1.3-.8h-4.1c-.6 0-1.1.3-1.3.8l-1.1 2.2c-.2.4-.5.7-1 .8-1.6.5-3.2 1.1-4.6 1.9-.4.2-.8.3-1.2.1l-2.3-.8c-.5-.2-1.1 0-1.5.4L5.9 8.8c-.4.4-.5 1-.4 1.5l.8 2.3c.1.4.1.8-.1 1.2-.8 1.5-1.5 3-1.9 4.7-.1.4-.4.8-.8 1l-2.1 1.1c-.5.3-.8.8-.8 1.3V26c0 .6.3 1.1.8 1.3l2.1 1.1c.4.2.7.5.8 1 .5 1.6 1.1 3.2 1.9 4.7.2.4.3.8.1 1.2l-.8 2.3c-.2.5 0 1.1.4 1.5L8.8 42c.4.4 1 .5 1.5.4l2.3-.8c.4-.1.8-.1 1.2.1 1.4.8 3 1.5 4.6 1.9.4.1.8.4 1 .8l1.1 2.2c.3.5.8.8 1.3.8h4.1c.6 0 1.1-.3 1.3-.8l1.1-2.2c.2-.4.5-.7 1-.8 1.6-.5 3.2-1.1 4.6-1.9.4-.2.8-.3 1.2-.1l2.3.8c.5.2 1.1 0 1.5-.4l2.9-2.9c.4-.4.5-1 .4-1.5l-.8-2.3c-.1-.4-.1-.8.1-1.2.8-1.5 1.5-3 1.9-4.7.1-.4.4-.8.8-1l2.1-1.1c.5-.3.8-.8.8-1.3v-4.1c.4-.5.1-1.1-.4-1.3zM24 41.5c-9.7 0-17.5-7.8-17.5-17.5S14.3 6.5 24 6.5 41.5 14.3 41.5 24 33.7 41.5 24 41.5z" fillRule="evenodd"></path></svg>;
 
-const UserPosts: React.FC<{username:string}> = ({username}) => {
+
+const UserPosts: React.FC<{username:string, type: 'posts'|'mentions'|'bookmarks'}> = ({username, type}) => {
     const dispatch = useDispatch();
-    let {data, isError, isLoading} = usePosts(username, 'posts');
+    let {data, isError, isLoading} = usePosts(username, type);
 
     if (data?.errors) {
         showAlert(dispatch, data.errors);
@@ -43,56 +44,6 @@ const UserPosts: React.FC<{username:string}> = ({username}) => {
         </React.Fragment>
     );
 }
-
-
-const UserBookmarks: React.FC<{username:string}> = ({username}) => {
-    const dispatch = useDispatch();
-    let {data, isError, isLoading} = usePosts(username, 'bookmarks');
-
-    if (data?.errors) {
-        showAlert(dispatch, data.errors);
-        data = null;
-    }
-
-    return (
-        <React.Fragment>
-            <div className='profile-posts'>
-                { isLoading ? <Spinner type='list' /> : ""}
-
-                <div role='user-posts' className='user-posts'>
-                    {data && <Posts view='tile' data={data}/>}
-                </div>
-            </div>
-
-        </React.Fragment>
-    );
-}
-
-
-const UserMentions: React.FC<{username:string}> = ({username}) => {
-    const dispatch = useDispatch();
-    let {data, isError, isLoading} = usePosts(username, 'mentions');
-
-    if (data?.errors) {
-        showAlert(dispatch, data.errors);
-        data = null;
-    }
-
-    return (
-        <React.Fragment>
-            <div className='profile-posts'>
-                { isLoading ? <Spinner type='list' /> : ""}
-
-                <div role='user-posts' className='user-posts'>
-                    {data && <Posts view='tile' data={data}/>}
-                </div>
-            </div>
-
-        </React.Fragment>
-    );
-}
-
-
 
 
 const UserProfile: React.FC<Router.RouteComponentProps> = ({match, location})=>{
@@ -221,9 +172,9 @@ const UserProfile: React.FC<Router.RouteComponentProps> = ({match, location})=>{
                                     <img id='icon' src='/icon/tag.png'/> <span className='__hide-mobile'>TAGGED</span>
                                 </label>
 
-                                <div className="content" id="content1"> {tab==0 && <UserPosts username={data.username}/>} </div>
-                                {isSelf && <div className="content" id="content2"> {tab==1 && <UserBookmarks username={data.username}/>} </div>}
-                                <div className="content" id="content3"> {tab==2 && <UserMentions username={data.username}/>} </div>
+                                <div className="content" id="content1"> {tab==0 && <UserPosts type='posts' username={data.username}/>} </div>
+                                {isSelf && <div className="content" id="content2"> {tab==1 && <UserPosts type='bookmarks' username={data.username}/>} </div>}
+                                <div className="content" id="content3"> {tab==2 && <UserPosts type='mentions' username={data.username}/>} </div>
                             </div>
                         </div>
 
