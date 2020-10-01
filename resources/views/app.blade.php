47a9ec1b200df5b536c08e536f467c8bb79b8ba2
kodejuice <kodejuice@gmail.com>
2020-10-01 15:15:13 +0100

    feat: meta csrf-token

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="robots" content="noimageindex, noarchive">
         <meta name="apple-mobile-web-app-status-bar-style" content="default">
         <meta name="mobile-web-app-capable" content="yes">
