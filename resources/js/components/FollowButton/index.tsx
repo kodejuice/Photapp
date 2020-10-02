5ae5e73c2e6e4638ccbb5687d6a72bb7ac5a42d0
kodejuice <kodejuice@gmail.com>
2020-10-02 19:17:41 +0100

    style: reduce loading svg size

--- resources/js/components/FollowButton/index.tsx
+++ resources/js/components/FollowButton/index.tsx
@@ -17,7 +17,7 @@ type FollowButtonProps = {
 }
 
 const loadingSmall = <svg className='load' style={{margin: '0 auto', display: 'block', 'shapeRendering': 'auto'}} width="13px" height="13px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"> <circle cx="50" cy="50" fill="none" stroke="#93dbe9" strokeWidth="10" r="35" strokeDasharray="164.93361431346415 56.97787143782138" transform="rotate(176.644 50 50)"> <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform> </circle> </svg>
-const loadingBig = <svg style={{margin: '0 auto', display: 'block', 'shapeRendering': 'auto'}} width="25px" height="25px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"> <circle cx="50" cy="50" fill="none" stroke="#93dbe9" strokeWidth="10" r="35" strokeDasharray="164.93361431346415 56.97787143782138" transform="rotate(176.644 50 50)"> <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform> </circle> </svg>
+const loadingBig = <svg style={{margin: '0 auto', display: 'block', 'shapeRendering': 'auto'}} width="20px" height="20px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"> <circle cx="50" cy="50" fill="none" stroke="#93dbe9" strokeWidth="10" r="35" strokeDasharray="164.93361431346415 56.97787143782138" transform="rotate(176.644 50 50)"> <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform> </circle> </svg>
 
 
 const FollowButton: React.FC<FollowButtonProps> = ({user, unfollow:_unfollow, notification_id, child, onFollow, onUnfollow}) => {
--- resources/js/components/RepostButton/index.tsx
+++ resources/js/components/RepostButton/index.tsx
@@ -10,6 +10,7 @@ type RepostButtonProps = {
     username: string,
 }
 
+const loading = <svg style={{margin: '0 auto', display: 'block', 'shapeRendering': 'auto'}} width="20px" height="20px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"> <circle cx="50" cy="50" fill="none" stroke="#93dbe9" strokeWidth="10" r="35" strokeDasharray="164.93361431346415 56.97787143782138" transform="rotate(176.644 50 50)"> <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform> </circle> </svg>
 
 const RepostButton: React.FC<RepostButtonProps> = ({post_id, username}) => {
     const dispatch = useDispatch();
