6a85c0c29b31f1ead83c6861609d78d387888db3
kodejuice <kodejuice@gmail.com>
2020-10-09 15:27:07 +0100

    feat: follow @ANON on registration

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 use App\User;
 use App\UserSetting;
+use App\UserFollow;
 
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -37,6 +38,8 @@ class ApiAuthController extends Controller
 
         $user = User::create($request->toArray());
         UserSetting::create(['user_id' => $user->id]);
+
+        $this->setupFollows($user->username);
 
         $token = $user->createToken($this->token_client)->accessToken;
 
@@ -85,5 +88,50 @@ class ApiAuthController extends Controller
         $token->revoke();
         $response = ['message' => 'You have been successfully logged out!'];
         return response($response, 200);
+    }
+
+
+    /////////////////
+    // User Follow //
+    /////////////////
+
+    // @ANON_USER follows regd user
+    // reg user follows @ANON
+
+    private function userFollow($user1_name, $user2_name) {
+        $user1 = User::firstWhere('username', $user1_name);
+        $user2 = User::firstWhere('username', $user2_name);
+
+        if (!$user1->first() || !$user2->first()) {
+            return;
+        }
+
+        $follows = UserFollow::where('user1_id', $user1->id)->where('user2_id', $user2->id);
+        if ($follows->first() || $user1->username == $user2->username) {
+            return;
+        }
+
+        $follow = new UserFollow();
+        $follow->user1_id = $user1->id;
+        $follow->user2_id = $user2->id;
+
+        $user1->follows += 1;
+        $user1->save();
+
+        $user2->followers += 1;
+        $user2->save();
+
+        $follow->save();
+    }
+
+
+    /**
+     * follow @ANON_USER
+     */
+    private function setupFollows($username) {
+        $anon = env("ANON_USER");
+
+        $this->userFollow($anon, $username);
+        $this->userFollow($username, $anon);
     }
 }
