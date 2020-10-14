949aeb40fccc16e617d17a6e86db608074e2ea44
kodejuice <kodejuice@gmail.com>
2020-10-14 19:58:31 +0100

    feat: is_email

--- resources/js/helpers/util.ts
+++ resources/js/helpers/util.ts
@@ -86,6 +86,16 @@ export function shuffle(items) {
 
 
 /**
+ * Determines whether the specified string is an email.
+ *
+ * @param      {string}  str     The string
+ */
+export function is_email(str: string) {
+    return str.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+}
+
+
+/**
  * memoizer
  *
  * we store the memoized values in a global map
