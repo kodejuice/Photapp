52dd76ebf072ea7a1bc57f684b5e73cfe4c017e3
kodejuice <kodejuice@gmail.com>
2020-10-18 04:14:03 +0100

    build: fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,19 +8,19 @@
         <meta name="mobile-web-app-capable" content="yes">
         <meta name="theme-color" content="#ffffff">
         <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" type="text/css" href="./css/app.css">
 
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
-        <link rel="manifest" href="{{ asset('manifest/manifest.json') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+        <link rel="manifest" href="./manifest/manifest.json">
 
         <meta property="og:site_name" content="PhotApp">
         <meta property="og:title" content="PhotApp">
-        <meta property="og:image" content="{{ asset('favicon/android-chrome-192x192.png') }}">
+        <meta property="og:image" content="./favicon/android-chrome-192x192.png">
 
-        <meta property="og:url" content="{{ asset('') }}">
-        <link rel="canonical" href="{{ asset('') }}">
+        <meta property="og:url" content="https://photapp-web.herokuapp.com">
+        <link rel="canonical" href="https://photapp-web.herokuapp.com">
 
         <meta content="A simple, fun photo and video sharing social network." name="description">
         <meta content="Biereagu Sochima" name="author">
@@ -30,6 +30,6 @@
     <body>
         <div id="root"> </div>
 
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="./js/app.js"></script>
     </body>
 </html>
